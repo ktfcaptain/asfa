b691eea7f11fc751450a7c25d87c4c4106279fa0
ktfcaptain <ktfcaptain@gmail.com>
2024-07-10 21:48:41 -0400

    removedcompress

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import tailwind from '@astrojs/tailwind';
-import compress from 'astro-compress';
 import icon from "astro-icon";
 
 import cloudflare from "@astrojs/cloudflare";
@@ -11,7 +10,7 @@ export default defineConfig({
   compressHTML: true,
   integrations: [mdx(), icon(), tailwind({
     applyBaseStyles: false
-  }), compress()],
+  })],
   output: "server",
   adapter: cloudflare()
 });
