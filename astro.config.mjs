d62b96c412757291243740275fd46218a86b72a1
ktfcaptain <ktfcaptain@gmail.com>
2024-07-10 22:47:13 -0400

    fuuuuuu

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import tailwind from '@astrojs/tailwind';
-import icon from "astro-icon";
+import compress from 'astro-compress';
 import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
